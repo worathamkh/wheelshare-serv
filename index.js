66767c0def5c495ae6916df4d0bf1a59c9b87849
Woratham Kh <kabutoboy@gmail.com>
2017-06-03 22:17:07 +0700

    listen when start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let firebase = require('firebase-admin');
+const firebase = require('firebase-admin');
 // let Promise = require('promise');
 // let escape = require('escape-html');
 // let serviceAccount = require('./secrets.json');
@@ -10,25 +10,29 @@ firebase.initializeApp({
   databaseURL: 'https://forward-vial-168614.firebaseio.com'
 });
 
-let express = require('express');
+const express = require('express');
 let app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (request, response) => {
-  response.json({ foo: 'bar' });
+app.get('/', (req, res) => {
+  res.json({ foo: 'bar' });
 });
 
-app.get('/api/all', (request, response) => {
-  firebase.database().ref('/')
-    .on('value',
-      (snapshot) => {
-        response.json(snapshot.val());
-      },
-      (error) => {
-        response.json({ error: error.code });
-      });
+let data = {};
+firebase.database().ref('/')
+  .on('value',
+    (snapshot) => {
+      data = snapshop.val();
+    });
+
+app.get('/api/all', (req, res) => {
+  res.json(data);
 });
+
+app.get('/api/shortest', (req, res) => {
+  console.log(JSON.stringify(res.query, null, 2));
+})
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
